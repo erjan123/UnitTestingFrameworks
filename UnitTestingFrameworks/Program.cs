a590f9bd1bbbb47d43b19fda423bea21e3a4db2c
erjan123 <erjan123@yahoo.com>
2018-12-19 21:04:35 +0200

    Add project files.

---
+++ UnitTestingFrameworks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTestingFrameworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
